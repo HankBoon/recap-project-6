03a0e99d40d2f66870c6f9c0302f5bca549b3fc9
Hauke <hank.la.boon@gmail.com>
2024-12-11 15:04:41 +0100

    add handler for get request

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,6 @@ import Card from "../components/Card";
 import useSWR from "swr";
 import { StyledLink } from "../components/StyledLink";
 
-const ListContainer = styled.ul`
-  list-style: none;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  gap: 20px;
-  width: 100%;
-`;
-const FixedLink = styled(StyledLink)`
-  position: fixed;
-  bottom: 50px;
-  right: 50px;
-`;
-
 export default function Home() {
   const { data } = useSWR("/api/places", { fallbackData: [] });
 
@@ -39,3 +26,16 @@ export default function Home() {
     </>
   );
 }
+
+const ListContainer = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 20px;
+  width: 100%;
+`;
+const FixedLink = styled(StyledLink)`
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+`;
